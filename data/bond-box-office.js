f2c18a0e2e98fbb7196be6387b6e937a7e44fb1a
Henok d <>
2022-08-20 21:07:45 +0000

    Added all the changes

--- data/bond-box-office.js
+++ data/bond-box-office.js
@@ -1,320 +1,90 @@
 {
-	"title": "Bond Films by Box Office Totals",
-	
+	"title": "Henok's Favorite Films In Order",
 	"films":[
-             {
-				"Film":23,
-				"Title":"Skyfall",
-				"Year":2012,
-				"Director":"Sam Mendes",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis, Robert Wade and John Logan",
-				"Composer":"Thomas Newman",
-				"Bond":"Daniel Craig",
-				"Budget":"$230,000,000.00",
-				"BoxOffice":"$1,110,526,981.00",
-				"Image":"skyfall.jpg"
-			  },
-              {
-				"Film":24,
-				"Title":"Spectre",
-				"Year":2015,
-				"Director":"Sam Mendes",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis, Robert Wade and John Logan",
-				"Composer":"Thomas Newman",
-				"Bond":"Daniel Craig",
-				"Budget":"$200,000,000.00",
-				"BoxOffice":"$879,620,923.00",
-				"Image":"spectre.jpg"
-			  },
-        
-              {
-				"Film":22,
-				"Title":"Quantum of Solace",
-				"Year":2008,
-				"Director":"Marc Forster",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis, Robert Wade and Paul Haggis",
-				"Composer":"David Arnold",
-				"Bond":"Daniel Craig",
-				"Budget":"$102,000,000.00",
-				"BoxOffice":"$594,420,283.00",
-				"Image":"quantum-of-solace.jpg"
-			  },
-			  {
-				"Film":21,
-				"Title":"Casino Royale",
-				"Year":2006,
-				"Director":"Martin Campbell",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis, Robert Wade and Paul Haggis",
-				"Composer":"David Arnold",
-				"Bond":"Daniel Craig",
-				"Budget":"$142,000,000.00",
-				"BoxOffice":"$431,942,139.00",
-				"Image":"casino-royale.jpg"
-			  },
-			  {
-				"Film":20,
-				"Title":"Die Another Day",
-				"Year":2002,
-				"Director":"Lee Tamahori",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis and Robert Wade",
-				"Composer":"David Arnold",
-				"Bond":"Pierce Brosnan",
-				"Budget":"$135,000,000.00",
-				"BoxOffice":"$361,730,660.00",
-				"Image":"die-another-day.jpg"
-			  },
-			  {
-				"Film":18,
-				"Title":"Tomorrow Never Dies",
-				"Year":1997,
-				"Director":"Roger Spottiswoode",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Bruce Feirstein",
-				"Composer":"David Arnold",
-				"Bond":"Pierce Brosnan",
-				"Budget":"$60,000,000.00",
-				"BoxOffice":"$356,429,941.00",
-				"Image":"tomorrow-never-dies.jpg"
-			  },
-			  {
-				"Film":19,
-				"Title":"The World Is Not Enough",
-				"Year":1999,
-				"Director":"Michael Apted",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis, Robert Wade and Bruce Feirstein",
-				"Composer":"David Arnold",
-				"Bond":"Pierce Brosnan",
-				"Budget":"$110,000,000.00",
-				"BoxOffice":"$339,504,276.00",
-				"Image":"the-world-is-not-enough.jpg"
-			  },
-			  {
-				"Film":11,
-				"Title":"Moonraker",
-				"Year":1979,
-				"Director":"Lewis Gilbert",
-				"Producers":"Albert R. Broccoli",
-				"Writers":"Christopher Wood",
-				"Composer":"John Barry",
-				"Bond":"Roger Moore",
-				"Budget":"$31,000,000.00",
-				"BoxOffice":"$210,300,000.00",
-				"Image":"moonraker.jpg"
-			  },
-			  {
-				"Film":12,
-				"Title":"For Your Eyes Only",
-				"Year":1981,
-				"Director":"John Glen",
-				"Producers":"Albert R. Broccoli",
-				"Writers":"Michael G. Wilson and Richard Maibaum",
-				"Composer":"Bill Conti",
-				"Bond":"Roger Moore",
-				"Budget":"$28,000,000.00",
-				"BoxOffice":"$195,300,000.00",
-				"Image":"for-your-eyes-only.jpg"
-			  },
-			  {
-				"Film":16,
-				"Title":"Licence to Kill",
-				"Year":1989,
-				"Director":"John Glen",
-				"Producers":"Albert R. Broccoli & Michael G. Wilson",
-				"Writers":"Michael G. Wilson and Richard Maibaum",
-				"Composer":"Michael Kamen",
-				"Bond":"Timothy Dalton",
-				"Budget":"$40,000,000.00",
-				"BoxOffice":"$191,200,000.00",
-				"Image":"license-to-kill.jpg"
-			  },
-			  {
-				"Film":13,
-				"Title":"Octopussy",
-				"Year":1983,
-				"Director":"John Glen",
-				"Producers":"Albert R. Broccoli",
-				"Writers":"George MacDonald Fraser, Michael G. Wilson and Richard Maibaum",
-				"Composer":"John Barry",
-				"Bond":"Roger Moore",
-				"Budget":"$27,500,000.00",
-				"BoxOffice":"$187,500,000.00",
-				"Image":"octopussy.jpg"
-			  },
-			  {
-				"Film":10,
-				"Title":"The Spy Who Loved Me",
-				"Year":1977,
-				"Director":"Lewis Gilbert",
-				"Producers":"Albert R. Broccoli",
-				"Writers":"Christopher Wood and Richard Maibaum",
-				"Composer":"Marvin Hamlisch",
-				"Bond":"Roger Moore",
-				"Budget":"$14,000,000.00",
-				"BoxOffice":"$185,400,000.00",
-				"Image":"the-spy-who-loved-me.jpg"
-			  },
-			  {
-				"Film":8,
-				"Title":"Live and Let Die",
-				"Year":1973,
-				"Director":"Guy Hamilton",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Tom Mankiewicz",
-				"Composer":"George Martin",
-				"Bond":"Roger Moore",
-				"Budget":"$7,000,000.00",
-				"BoxOffice":"$161,800,000.00",
-				"Image":"live-and-let-die.jpg"
-			  },
-			  {
-				"Film":14,
-				"Title":"A View to a Kill",
-				"Year":1985,
-				"Director":"John Glen",
-				"Producers":"Albert R. Broccoli & Michael G. Wilson",
-				"Writers":"Michael G. Wilson and Richard Maibaum",
-				"Composer":"John Barry",
-				"Bond":"Roger Moore",
-				"Budget":"$36,000,000.00",
-				"BoxOffice":"$160,000,000.00",
-				"Image":"a-view-to-a-kill.jpg"
-			  },
-			  {
-				"Film":17,
-				"Title":"GoldenEye",
-				"Year":1995,
-				"Director":"Martin Campbell",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Michael France, Jeffrey Caine, Kevin Wade and Bruce Feirstein",
-				"Composer":"Éric Serra",
-				"Bond":"Pierce Brosnan",
-				"Budget":"$42,000,000.00",
-				"BoxOffice":"$156,167,015.00",
-				"Image":"goldeneye.jpg"
-			  },
-			  {
-				"Film":15,
-				"Title":"The Living Daylights",
-				"Year":1987,
-				"Director":"John Glen",
-				"Producers":"Albert R. Broccoli & Michael G. Wilson",
-				"Writers":"Michael G. Wilson and Richard Maibaum",
-				"Composer":"John Barry",
-				"Bond":"Timothy Dalton",
-				"Budget":"$30,000,000.00",
-				"BoxOffice":"$152,627,960.00",
-				"Image":"the-living-daylights.jpg"
-			  },
-			  {
-				"Film":4,
-				"Title":"Thunderball",
-				"Year":1965,
-				"Director":"Terence Young",
-				"Producers":"Kevin McClory",
-				"Writers":"Richard Maibaum and John Hopkins",
-				"Composer":"John Barry",
-				"Bond":"Sean Connery",
-				"Budget":"$9,000,000.00",
-				"BoxOffice":"$141,200,000.00",
-				"Image":"thunderball.jpg"
-			  },
-			  {
-				"Film":3,
-				"Title":"Goldfinger",
-				"Year":1964,
-				"Director":"Guy Hamilton",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Richard Maibaum and Paul Dehn",
-				"Composer":"John Barry",
-				"Bond":"Sean Connery",
-				"Budget":"$3,000,000.00",
-				"BoxOffice":"$124,900,000.00",
-				"Image":"goldfinger.jpg"
-			  },
-			  {
-				"Film":7,
-				"Title":"Diamonds Are Forever",
-				"Year":1971,
-				"Director":"Guy Hamilton",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Richard Maibaum and Tom Mankiewicz",
-				"Composer":"John Barry",
-				"Bond":"Sean Connery",
-				"Budget":"$7,200,000.00",
-				"BoxOffice":"$116,000,000.00",
-				"Image":"diamonds-are-forever.jpg"
-			  },
-			  {
-				"Film":5,
-				"Title":"You Only Live Twice",
-				"Year":1967,
-				"Director":"Lewis Gilbert",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Roald Dahl",
-				"Composer":"John Barry",
-				"Bond":"Sean Connery",
-				"Budget":"$9,500,000.00",
-				"BoxOffice":"$111,600,000.00",
-				"Image":"you-only-live-twice.jpg"
-			  },
-			  {
-				"Film":9,
-				"Title":"The Man with the Golden Gun",
-				"Year":1974,
-				"Director":"Guy Hamilton",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Richard Maibaum and Tom Mankiewicz",
-				"Composer":"John Barry",
-				"Bond":"Roger Moore",
-				"Budget":"$7,000,000.00",
-				"BoxOffice":"$97,600,000.00",
-				"Image":"the-man-with-the-golden-gun.jpg"
-			  },
-			  {
-				"Film":6,
-				"Title":"On Her Majesty's Secret Service",
-				"Year":1969,
-				"Director":"Peter R. Hunt",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Richard Maibaum",
-				"Composer":"John Barry",
-				"Bond":"George Lazenby",
-				"Budget":"$8,000,000.00",
-				"BoxOffice":"$82,000,000.00",
-				"Image":"on-her-majestys-secret-service.jpg"
-			  },
-			  {
-				"Film":2,
-				"Title":"From Russia with Love",
-				"Year":1963,
-				"Director":"Terence Young",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Richard Maibaum and Johanna Harwood",
-				"Composer":"John Barry",
-				"Bond":"Sean Connery",
-				"Budget":"$2,000,000.00",
-				"BoxOffice":"$78,900,000.00",
-				"Image":"from-russia-with-love.jpg"
-			  },
-			  {
+			      {
 				"Film":1,
-				"Title":"Dr. No",
-				"Year":1962,
-				"Director":"Terence Young",
-				"Producers":"Harry Saltzman and Albert R. Broccoli",
-				"Writers":"Richard Maibaum, Johanna Harwood and Berkely Mather",
-				"Composer":"Monty Norman",
-				"Bond":"Sean Connery",
-				"Budget":"$1,000,000.00",
-				"BoxOffice":"$59,567,035.00",
-				"Image":"dr-no.jpg"
-			  }
+				"Title":"The Shawshank Redemption",
+				"Year":1994,
+				"Director":"Frank Darabont",
+				"Writers":"Stephen King and Frank Darabont",
+				"Actors":"Tim Robbins, Morgan Freeman, and Bob Gunton"
+			  },
+  {
+				"Film":2,
+				"Title":"The Godfather",
+				"Year":1972,
+				"Director":"Francis Ford Coppola",
+				"Writers":"Mario Puzo and Francis Ford Coppola",
+				"Actors":"Marlon Brando, Al Pacino, and James Caan"
+			  },
+ {
+				"Film":3,
+				"Title":"The Dark Knight",
+				"Year":2008,
+				"Director":"Christopher Nolan",
+				"Writers":"Jonathan Nolan, Christopher Nolan, and David S. Goyer",
+				"Actors":"Sean Connery"
+			  },
+  {
+				"Film":4,
+				"Title":"The Godfather Part II",
+				"Year":1974,
+				"Director":"Francis Ford Coppola",
+				"Writers":"Francis Ford Coppola and Mario Puzo",
+				"Actors":"Al Pacino, Robert De Niro, and Robert Duvall"
+			  },
+{
+				"Film":5,
+				"Title":"12 Angry Men",
+				"Year":1957,
+				"Director":"Sidney Lumet ",
+				"Writers":"Reginald Rose",
+				"Actors":"Henry Fonda, Lee J. Cobb, and Martin Balsam"
+			  },
+  {
+				"Film":6,
+				"Title":"Schindler's List",
+				"Year":1993,
+				"Director":"Steven Spielberg ",
+				"Writers":" Thomas Keneally and Steven Zaillian",
+				"Actors":"Liam Neeson, Ralph Fiennes, and Ben Kingsley "
+			  },
+{
+				"Film":7,
+				"Title":"The Lord of the Rings: The Return of the King",
+				"Year":2003,
+				"Director":"Peter Jackson",
+				"Writers":"J.R.R. Tolkien, Fran Walsh, Philippa Boyens",
+				"Actors":"Elijah Wood, Viggo Mortensen, and Ian McKellen"
+			  },
+ {
+				"Film":8,
+				"Title":"Pulp Fiction",
+				"Year":1994,
+				"Director":"Quentin Tarantino",
+				"Writers":"Quentin Tarantino and Roger Avary",
+				"Actors":"John Travolta, Uma Thurman, and Samuel L. Jackson"
+			  },
+  {
+ 				"Film":9,
+				"Title":"The Lord of the Rings: The Fellowship of the Ring",
+				"Year":2001,
+				"Director":"Peter Jackson",
+				"Writers":"J.R.R. Tolkien, Fran Walsh, and Philippa Boyens",
+				"Actors":"Elijah Wood, Ian McKellen, and Orlando Bloom"
+			  },
+{
+ 				"Film":10,
+				"Title":"The Good, the Bad and the Ugly",
+				"Year":1966,
+				"Director":"Sergio Leone",
+        "Writers":"Luciano Vincenzoni, Sergio Leone, and Agenore Incrocci",
+				"Actors":"Clint Eastwood, Eli WallachLee, and Van Cleef"
 
+			  }
+ 			 			   					 
 			]
 }
+
+
+
--- index.php
+++ index.php
@@ -1,32 +1,105 @@
 <html>
 <head>
-<title>Bond Web Service Demo</title>
+<title>Henok's Top 10 Movies</title>
 <style>
-	body {font-family:georgia;}
+	body {font-family:georgia}
+
+  .film{
+    border:1px solid #E77DC2;
+    border-radius: 10px;
+    padding: 5px;
+    margin-bottom:5px;
+    position:relative;   
+  }
+ 
+  .pic{
+    position:absolute;
+    right:10px;
+    top:10px;
+  }
+  
+  .pic img{
+	max-width:50px;
+  }
+
 </style>
+  
 <script src="https://code.jquery.com/jquery-latest.js"></script>
 
 <script type="text/javascript">
-$(document).ready(function() {  
 
-	$('.category').click(function(e){
-        e.preventDefault(); //stop default action of the link
-		cat = $(this).attr("href");  //get category from URL
-		alert(cat);
-	});
-});	
+function bondTemplate(film){
+  
+return `
+  
+  <div class="film">
+      <b>Film</b>: ${film.Film}<br /> 
+      <b>Title</b>: ${film.Title}<br /> 
+      <b>Year</b>: ${film.Year}<br /> 
+      <b>Director</b>: ${film.Director}<br /> 
+      <b>Writers</b>: ${film.Writers}<br /> 
+      <b>Actors</b>: ${film.Actors}<br />   
+    </div>
+  
+    `; 
+  
+  }
+
+
+  
+$(document).ready(function() { 
+ 
+ $('.category').click(function(e){
+   e.preventDefault(); //stop default action of the link
+   cat = $(this).attr("href");  //get category from URL
+  
+   var request = $.ajax({
+     url: "api.php?cat=" + cat,
+     method: "GET",
+     dataType: "json"
+   });
+   request.done(function( data ) {
+     console.log(data);
+
+    $("#filmtitle").html(data.title);
+
+    $("#films").html("");
+
+     
+    $.each(data.films,function(i,item){
+          let myData =  bondTemplate(item);
+         $("<div></div>").html(myData).appendTo("#films");
+      });
+
+     $("#output").html(myData);
+      
+/*      // let myData = JSON.stringify(data,null,4);
+     //  myData = "<pre>" + myData + "</pre>";
+     // $("#output").html(myData); */
+
+   });
+   request.fail(function(xhr, status, error ) {
+alert('Error - ' + xhr.status + ': ' + xhr.statusText);
+   });
+ 
+  });
+}); 
+
 
 
 </script>
 </head>
 	<body>
-	<h1>Bond Web Service</h1>
-		<a href="year" class="category">Bond Films By Year</a><br />
-		<a href="box" class="category">Bond Films By International Box Office Totals</a>
+	<h1>Henok's Top 10 Movies</h1>
+		<a href="year" class="category">My Favorite Films By Year</a><br />
+		<a href="box" class="category">My Favorite Films In Order</a>
 		<h3 id="filmtitle">Title Will Go Here</h3>
 		<div id="films">
-			<p>Films will go here</p>
+
+
 		</div>
+
+
 		<div id="output">Results go here</div>
 	</body>
 </html>
